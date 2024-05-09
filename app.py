37612f0df3358415ba175c9fae6dc8f3020599ca
asrith-reddy <76733972+asrith-reddy@users.noreply.github.com>
2024-05-09 16:19:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,20 +22,21 @@ def home():
     return render_template("index.html")
 @app.route('/result',methods=['POST','GET'])
 def predict():
-    url = request.form["name"]
-    obj = FeatureExtraction(url)
-    x = np.array(obj.getFeaturesList()).reshape(1,30)
-
-    y_pred =gbc.predict(x)[0]
-        #1 is safe
-        #-1 is unsafe
-    #y_pro_phishing = gbc.predict_proba(x)[0,0]
-    #y_pro_non_phishing = gbc.predict_proba(x)[0,1]
-        # if(y_pred ==1 ):
-    #3pred = "It is {0:.2f} % safe to go ".format(y_pro_phishing*100)
-    #xx =y_pred
-    name=convertion(url,int(y_pred))
-    return render_template("index.html", name=name)
+    if request.method == "POST":
+        url = request.form["name"]
+        obj = FeatureExtraction(url)
+        x = np.array(obj.getFeaturesList()).reshape(1,30)
+    
+        y_pred =gbc.predict(x)[0]
+            #1 is safe
+            #-1 is unsafe
+        #y_pro_phishing = gbc.predict_proba(x)[0,0]
+        #y_pro_non_phishing = gbc.predict_proba(x)[0,1]
+            # if(y_pred ==1 ):
+        #3pred = "It is {0:.2f} % safe to go ".format(y_pro_phishing*100)
+        #xx =y_pred
+        name=convertion(url,int(y_pred))
+        return render_template("index.html", name=name)
 @app.route('/usecases', methods=['GET', 'POST'])
 def usecases():
     return render_template('usecases.html')
